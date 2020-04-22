6d1a4edc08be9f9d1a98dd116d34360fce90a3eb
Brooke Nemchak <bnemchak@gmail.com>
2020-04-21 19:46:17 -0500

    instructors

--- loops.js
+++ loops.js
@@ -15,8 +15,36 @@ const colorLoop = () => {
 }
 
 
+//const init = () => {
+  //colorLoop ();
+//}
+
+//init();
+
+// Exercise 1:
+// create an array of instructor objects, each with firstName and lastName properties (keys/values)
+// create a function that will loop over the instructors array
+// console.log() full instructor names i.e. "Luke Lancaster", "Matt Gill", etc etc etc etc
+// call the function in init()
+
+
+
+const instructors = [
+  { firstName: 'Luke', lastName: 'Lancaster'},
+  { firstName: 'Matt', lastName: 'Gill'},
+  { firstName: 'Michael', lastName: 'Clark'},
+];
+
+const instructorsLoop = () => {
+  let domString = '';
+   for (let i = 0; i < instructors.length; i++) {
+     domString += `<h2>${instructors[i].firstName} ${instructors[i].lastName}</h2>`;
+   }
+   console.log(domString)
+}
+
 const init = () => {
-  colorLoop ();
+  instructorsLoop();
 }
 
 init();
